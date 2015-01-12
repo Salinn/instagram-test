c544bb95d481042dd7a5cfe625a8272443e6991c
Tall Paul <pmd6624@rit.edu>
2015-01-12 13:19:55 -0500

    instagram pics pulled in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-
+    @instagram = Instagram.user_recent_media(current_user.authentications.where(provider: "instagram").first.uid, {:count => 3})
   end
 end
